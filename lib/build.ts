6e966e3b85576041ca6d67a78c2b1e61336efec4
EC2 Default User <ec2-user@ip-172-31-76-140.ec2.internal>
2023-10-23 15:17:20 +0000

    random simulations

--- lib/build.ts
+++ lib/build.ts
@@ -1,30 +1,66 @@
 import { Board } from './Board';
 
-let ITERATIONS = 50;
+let ITERATIONS = 500;
 
 var x: string[][] = [
-            ['W','','','','Y',''],
-            ['','','W','','M',''],
-            ['','','W','','C',''],
-            ['','','W','','G',''],
-            ['','','','','R',''],
-            ['','B','','','',''],
+            [' ' ,' ' ,' ' ,' ' ,' ' ,' ' ],
+            [' ' ,'B' ,'B' ,' ' ,' ' ,' ' ],
+            [' ' ,'B' ,'B','B' ,' ' ,' ' ],
+            [' ' ,'' ,'B' ,'B',' ' ,' ' ],
+            [' ' ,'','','',' ' ,' ' ],
+            [' ' ,' ' ,' ' ,' ' ,' ' ,' ' ],
         ];
         
 var y: string[][] = [
-            ['C' ,'C' ,'C' ,'C' ,'C' ,'C' ],
-            ['C' ,'C' ,'C' ,'C' ,'C' ,'C' ],
-            ['C' ,'C' ,'R','C' ,'C' ,'C' ],
-            ['C' ,'C' ,'C' ,'R','C' ,'C' ],
-            ['C' ,'R','R','R','C' ,'C' ],
-            ['C' ,'C' ,'C' ,'C' ,'C' ,'C' ],
+            [' ' ,' ' ,' ' ,' ' ,' ' ,' ' ],
+            [' ' ,' ' ,' ' ,' ' ,' ' ,' ' ],
+            [' ' ,' ' ,'W',' ' ,' ' ,' ' ],
+            [' ' ,' ' ,' ' ,'W',' ' ,' ' ],
+            [' ' ,'W','W','W',' ' ,' ' ],
+            [' ' ,' ' ,' ' ,' ' ,' ' ,' ' ],
         ];
+        
+var z: string[][] = [
+            ['W','','','','Y',''],
+            ['','','W','','M',''],
+            ['','','W','',' ',''],
+            ['','','W','','G',''],
+            ['','','','','W',''],
+            ['','B','','','',''],
+        ];
+        
+let auto: Array<Array<string>> = new Array<Array<string>>();
 
-
-let board = new Board(y);
-board.print();
-
-for (var i = 0; i<ITERATIONS; i++){
-    board.nextStep();
-    board.print(); 
+for(let i = 0; i<25; i++){
+    auto.push(new Array<string>())
+    for(let j = 0; j<25; j++){
+        if (i>10 && j>10 && j<23 && i<23) {
+            let rand = Math.floor(Math.random() * 10);
+            if (rand == 1) auto[i].push('W');
+            else if (rand == 2) auto[i].push('W');
+            else if (rand == 3) auto[i].push('W');
+            else if (rand == 4) auto[i].push('W');
+            else if (rand == 5) auto[i].push('W');
+            else if (rand == 6) auto[i].push('W');
+            else if (rand == 7) auto[i].push('W');
+            else auto[i].push('');
+        }
+        else {
+            auto[i].push('');
+        }
+    }
 }
+
+const sleep = (waitTimeInMs: number) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+let board = new Board(auto);
+board.print();
+let i = 0 
+function next() {
+    board.nextStep();
+    console.clear();
+    board.print(); 
+    sleep(500).then(() => {i++; if(i<ITERATIONS) next();})
+}
+
+next();
