4d8f8e621ade9eeec6f5f2a56a016cbd74e54a27
Brian <brian.lynch4444@gmail.com>
2023-10-27 12:16:48 -0400

    paths in server

--- webassembly/cmd/server/main.go
+++ webassembly/cmd/server/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
+func getA(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
 func main() {
 	fmt.Println("Attempting to start server...")
-	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../../assets")))
+	http.HandleFunc("/hello", getA)
+	//err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../../assets")))
+	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 		return
